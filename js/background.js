bda23a8142f37f528a5cebf4384111d2eea0c1ca
Ruiwen Chua <ruiwen+github@thoughtmonkeys.com>
2013-04-14 23:48:02 +0800

    Cleaning up unnecessary comments

--- js/background.js
+++ js/background.js
@@ -27,19 +27,13 @@ var notify = {
 	utils: {
 		ab2str: function(buf) {
 			// http://updates.html5rocks.com/2012/06/How-to-convert-ArrayBuffer-to-and-from-String
-			// FUTURE: https://code.google.com/p/stringencoding/
-			//return String.fromCharCode.apply(null, new Uint16Array(buf));
+			// https://code.google.com/p/stringencoding/
+
 			return TextDecoder('utf-8').decode(new Uint8Array(buf));
 		},
 		str2ab: function(str) {
 			// http://updates.html5rocks.com/2012/06/How-to-convert-ArrayBuffer-to-and-from-String
-			// FUTURE: https://code.google.com/p/stringencoding/
-			// var buf = new ArrayBuffer(str.length*2); // 2 bytes for each char
-			// var bufView = new Uint16Array(buf);
-			// for (var i=0, strLen=str.length; i<strLen; i++) {
-			// 	bufView[i] = str.charCodeAt(i);
-			// }
-			// return buf;
+			// https://code.google.com/p/stringencoding/
 
 			return TextEncoder('utf-8').encode(str);
 		},
@@ -205,17 +199,6 @@ var notify = {
 	socket: {
 		list: {},
 		open: function(tag, port, cb) {
-			//console.log(chrome);
-			// chrome.socket.create('udp', '127.0.0.1', 9000, { onEvent: notify.socket.onData },
-   //              function(socketInfo) {
-   //              	var socketId = socketInfo.socketId;
-   //              	chrome.socket.connect(socketId, function(result) {
-   //              		console.log("connected!");
-   //              		var notification = webkitNotifications.createNotification("",
-   //              			"Notify", "Socket open");
-   //              		notification.show();
-   //              	});
-			// });
 
 			// USEFUL: Chrome socket api error list:
 			// https://code.google.com/p/chromium/codesearch#chromium/src/net/base/net_error_list.h
@@ -243,10 +226,6 @@ var notify = {
 						if(typeof(cb) == 'function') {
 							cb(socketId);
 						}
-
-						// var notification = webkitNotifications.createNotification("",
-						// 	"Notify", "Socket open");
-						// notification.show();
 
 						notify.socket.receiveLoop(socketId, 512, notify.socket.onData);
 					}
@@ -289,12 +268,6 @@ chrome.app.runtime.onLaunched.addListener(function() {
 	});
 
 });
-
-// chrome.app.runtime.onRestarted.addListener(function() {
-// 	notify.socket.list.forEach(function(s) {
-// 		chrome.socket.destroy(s);
-// 	});
-// });
 
 notify.socket.open("main");
 notify.key.retrieve();  // Retrieve the password
